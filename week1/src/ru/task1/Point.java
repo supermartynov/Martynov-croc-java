0750ebdb26d9fa7ac565594209f2c87e77ad2c67
Daniil Martynov <55408414+supermartynov@users.noreply.github.com>
2021-10-08 18:21:49 +0300

    Add files via upload

---
+++ week1/src/ru/task1/Point.java
@@ -0,0 +1,32 @@
+package ru.croc.task1;
+
+public class Point {
+
+    private double x;
+
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
