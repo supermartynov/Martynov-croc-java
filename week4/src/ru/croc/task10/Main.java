0cd163b2b35e5af3f7050e15b5e3398b1ba7398b
supermartynov <suppermartynov@gmail.com>
2021-11-05 17:38:35 +0300

    task 10

---
+++ week4/src/ru/croc/task10/Main.java
@@ -0,0 +1,46 @@
+package ru.croc.task10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> callsHistory = new ArrayList<>();
+        callsHistory.add("1,12");
+        callsHistory.add("4,6");
+        callsHistory.add("7,12");
+        callsHistory.add("10,32");
+        callsHistory.add("15,30");
+
+        List<String> startTimeArray = new ArrayList<>();
+        List<String> endTimeArray = new ArrayList<>();
+
+        for (String str : callsHistory) {
+            String[] stringArr = str.split(",");
+            startTimeArray.add(stringArr[0]);
+            endTimeArray.add(stringArr[1]);
+        }
+
+        int maxCalls = 0;
+        Long currentStartTime = 0L;
+        Long currentEndTime = 0L;
+
+        for (int i = 0; i < callsHistory.size(); i++) {
+            int tmpMaxCalls = 0;
+            currentStartTime = Long.parseLong(startTimeArray.get(i));
+            currentEndTime = Long.parseLong(endTimeArray.get(i));
+            for (int j = 0; j < callsHistory.size(); j++) {
+                if (Long.parseLong(startTimeArray.get(j)) >= currentStartTime && Long.parseLong(endTimeArray.get(j)) <= currentEndTime) {
+                    tmpMaxCalls ++;
+                }
+            }
+            if (tmpMaxCalls > maxCalls) {
+                maxCalls = tmpMaxCalls;
+            }
+        }
+
+        System.out.println(maxCalls);
+    }
+}
