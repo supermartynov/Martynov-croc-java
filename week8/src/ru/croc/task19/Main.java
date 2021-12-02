9a0eda5528ab0388cb5cbe985f3b17e4fbc37dc1
supermartynov <suppermartynov@gmail.com>
2021-12-02 10:33:04 +0300

    task 19

--- week8/src/ru/croc/task19/Main.java
+++ week8/src/ru/croc/task19/Main.java
@@ -7,12 +7,9 @@ public class Main {
     public final static String filePath = "./week8/src/ru/croc/task19/output.txt";
 
     public static void main(String[] args) throws IOException {
-        try (FileWriter fileWriter = new FileWriter(filePath);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)))
+        try (FileWriter fileWriter = new FileWriter(filePath))
         {
-            System.out.println("Введите что-нибудь для записи в текстовый файл");
-            String text = bufferedReader.readLine();
-            fileWriter.write(text);
+            fileWriter.write("Hello, world!");
         }
     }
 }
